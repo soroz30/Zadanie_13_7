b6c6a7c6ccf48587b438daece3a258db1e51adff
soroz30 <soroz30@yahoo.pl>
2017-10-26 18:35:24 +0200

    correction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var colors = require('colors');
 
 fs.readdir('./', function(err, data) {
-	if (err) throw err;
+   if (err) throw err;
     var files = data.join(', ');
     fs.writeFile('files.txt', files, function(err) {
         if (err) throw err;
