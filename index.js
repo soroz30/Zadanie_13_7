cfb4633aea6b968d794e8dfbfef3d52fe8e7ad0e
soroz30 <soroz30@yahoo.pl>
2017-10-26 18:33:49 +0200

    correction

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var colors = require('colors');
 
 fs.readdir('./', function(err, data) {
-    files = data.join(', ');
+	if (err) throw err;
+    var files = data.join(', ');
     fs.writeFile('files.txt', files, function(err) {
         if (err) throw err;
         console.log('Zapisano'.green);
